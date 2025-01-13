64abed005bcaa5de8e61763b0f7dc6b0d6b7ba2d
sahan-chinthaka <sahanchinthaka04@gmail.com>
2025-01-13 15:44:52 +0530

    Update NavBar component

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,16 +1,36 @@
-import { Link, NavLink } from "react-router-dom";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Link, useLocation } from "react-router-dom";
 
 function NavBar() {
+  const location = useLocation();
+
   return (
     <header className="flex items-baseline border-b bg-white p-5">
       <nav>
-        <ul className="flex gap-2">
+        <ul className="flex gap-4">
           <li>
-            <NavLink to="/">Home</NavLink>
+            <Link
+              to="/"
+              className={cn(
+                "hover:text-primary/50 font-semibold text-gray-400",
+                location.pathname == "/" && "text-primary hover:text-primary",
+              )}
+            >
+              HOME
+            </Link>
           </li>
           <li>
-            <NavLink to="/books">Books</NavLink>
+            <Link
+              to="/books"
+              className={cn(
+                "hover:text-primary/50 font-semibold text-gray-400",
+                location.pathname.startsWith("/books") &&
+                  "text-primary hover:text-primary",
+              )}
+            >
+              BOOKS
+            </Link>
           </li>
         </ul>
       </nav>
